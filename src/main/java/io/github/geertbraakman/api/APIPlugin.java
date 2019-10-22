26bbb877d84ca0b24a45b331e5696edac45120a5
Geert Braakman <geert.braakman@gmail.com>
2019-10-22 09:30:43 +0200

    Removed some left-overs from the StorageHandler

--- src/main/java/io/github/geertbraakman/api/APIPlugin.java
+++ src/main/java/io/github/geertbraakman/api/APIPlugin.java
@@ -4,7 +4,6 @@ import io.github.geertbraakman.api.command.CommandHandler;
 import io.github.geertbraakman.api.config.ConfigHandler;
 import io.github.geertbraakman.api.messaging.MessageHandler;
 import io.github.geertbraakman.api.reloading.Reloader;
-import io.github.geertbraakman.api.storage.StorageHandler;
 import io.github.geertbraakman.api.userinterface.UserInterfaceHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,7 +13,6 @@ public class APIPlugin extends JavaPlugin {
     private CommandHandler commandHandler;
     private ConfigHandler configHandler;
     private Reloader reloader;
-    private StorageHandler storageHandler;
     private UserInterfaceHandler userInterfaceHandler;
 
     public MessageHandler getMessageHandler() {
@@ -46,13 +44,6 @@ public class APIPlugin extends JavaPlugin {
         }
 
         return reloader;
-    }
-
-    public StorageHandler getStorageHandler() {
-        if (storageHandler == null) {
-            storageHandler = new StorageHandler(this);
-        }
-        return storageHandler;
     }
 
     public UserInterfaceHandler getUserInterfaceHandler() {
